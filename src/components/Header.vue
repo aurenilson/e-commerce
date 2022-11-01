181aca8e288999638226fc923ab080b35f4ee0d2
Eduardo Mauricio Sehn <eduardosehn0@gmail.com>
2022-11-01 17:47:05 -0300

    teste 2

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
         <div class= "uk-grid">
             <div class="uk-width-1-2"><a href=""></a></div>
             <div class="uk-width-1-2"><a href="#">Home</a></div>
-            <div class="uk-width-1-2"> <a href="#">Cadastro</a></div>
+            <div class="uk-width-1-2"> <a href="#">Cadastroooo</a></div>
         </div>
 
     </div>
