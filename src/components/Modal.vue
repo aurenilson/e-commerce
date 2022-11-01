09d73d2630c031a8038bf61d029a7bc8454d69c6
Helena Della Giustina <helenadgiustina@gmail.com>
2022-11-01 17:56:39 -0300

    Inicio home

---
+++ src/components/Modal.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <button
+            uk-toggle="target: #my-id"
+            type="button"
+            class="uk-button uk-button-primary"
+        >
+            Abrir Modal
+        </button>
+        <!-- This is the modal -->
+        <div id="my-id" uk-modal>
+            <div class="uk-modal-dialog uk-modal-body">
+                <h2 class="uk-modal-title">Isso é um Modal</h2>
+                <div class="uk-padding-large">
+                    <!-- <RouterView /> -->
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Modal'
+    }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,28 @@
-<script setup>
-</script>
-
 <template>
     <main>
-        <TheWelcome />
+        <section>
+            <!-- imagem e parte de acrescentar os produtos -->
+            <!-- <img src="" alt=""> -->
+            <div class="uk-card uk-card-default uk-card-body">
+                <p>Produtos</p>
+            </div>
+        </section>
+        <section>
+            <!-- carrossel de imagens -->
+        </section>
+        <article>
+            <!-- texto sobre os produtos -->
+        </article>
+        <Modal />
     </main>
 </template>
+
+
+<script >
+import Modal from "../components/Modal.vue"
+    export default {
+        components: {
+            Modal
+        }
+    }
+</script>
