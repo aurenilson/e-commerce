085b56236a2db8aeb20fef417271ef59eb913c76
Johanes Rusch <jrusch@imply.com>
2022-11-01 17:16:09 -0300

    INiciando Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+   <div class="uk-container">
+      <div class="uk-grid">
+         <div class="uk-width-4-5"><a href="#">Home</a></div>
+         <div class="uk-width-4-5"><a href="#">Institucional</a></div>
+         <div class="uk-width-4-5"><a href="#">Políticas de cookies</a></div>
+         <div class="uk-width-4-5"><a href="#">Políticas de privacidade</a></div>
+         <div class="uk-width-1-5" uk-height-viewport="offset-top: true"><img src="/img/parque_das_palmeiras.png" alt="Logo do site"></div>
+      </div>
+   </div>
+</template>
+<script>
+   export default {
+      name: 'Footer'
+   }
+</script>
