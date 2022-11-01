837707bdac7c95c62211960e2679b889c033e227
Eduardo Mauricio Sehn <eduardosehn0@gmail.com>
2022-11-01 17:17:21 -0300

    criação do header

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,99 @@
 <template>
+    <header>
+        <div class="wrapper">
+
+            <nav>
+                <RouterLink to="/">Home</RouterLink>
+                <RouterLink to="/about">About</RouterLink>
+            </nav>
+        </div>
+    </header>
+
     <div>
-        <Footer />
+        <button
+            uk-toggle="target: #my-id"
+            type="button"
+            class="uk-button uk-button-primary"
+        >
+            Abrir Modal
+        </button>
+        <!-- This is the modal -->
+        <div id="my-id" uk-modal>
+            <div class="uk-modal-dialog uk-modal-body">
+                <h2 class="uk-modal-title">Isso é um Modal</h2>
+                <div class="uk-padding-large">
+                    <RouterView />
+                </div>
+            </div>
+        </div>
     </div>
+
 </template>
 
-<script>
-// import { RouterLink, RouterView } from "vue-router"
-import Footer from "./components/Footer.vue"
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
 
-export default {
-    components: {
-        Footer
+<style scoped>
+header {
+    line-height: 1.5;
+    max-height: 100vh;
+}
+
+.logo {
+    display: block;
+    margin: 0 auto 2rem;
+}
+
+nav {
+    width: 100%;
+    font-size: 12px;
+    text-align: center;
+    margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+    color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+    background-color: transparent;
+}
+
+nav a {
+    display: inline-block;
+    padding: 0 1rem;
+    border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+    border: 0;
+}
+
+@media (min-width: 1024px) {
+    header {
+        display: flex;
+        place-items: center;
+        padding-right: calc(var(--section-gap) / 2);
+    }
+
+    .logo {
+        margin: 0 2rem 0 0;
+    }
+
+    header .wrapper {
+        display: flex;
+        place-items: flex-start;
+        flex-wrap: wrap;
+    }
+
+    nav {
+        text-align: left;
+        margin-left: -1rem;
+        font-size: 1rem;
+
+        padding: 1rem 0;
+        margin-top: 1rem;
     }
 }
-</script>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template uk-container>
+    <div>
+        <div class= "uk-grid">
+            <div class="uk-width-1-2"><a href=""></a></div>
+            <div class="uk-width-1-2"><a href="#">Home</a></div>
+            <div class="uk-width-1-2"> <a href="#">Cadastro</a></div>
+        </div>
+
+    </div>
+</template>
+<script>
+export default{
+    name: "Header"
+}
+</script>
