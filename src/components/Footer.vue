b32977a361e274dbc0b04dfab47f23bbad806256
johanes-rusch <johanesrusch@gmail.com>
2022-11-01 20:09:52 -0300

    Alguns estilos

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,11 @@
 <template>
    <div class="uk-container">
-      <div class="uk-grid uk-background-muted">
-         <div class="uk-width-4-5"><a href="#">Home</a></div>
-         <div class="uk-width-4-5"><a href="#">Institucional</a></div>
-         <div class="uk-width-4-5"><a href="#">Políticas de cookies</a></div>
-         <div class="uk-width-4-5"><a href="#">Políticas de privacidade</a></div>
-         <div class="uk-width-1-5 un-position-top uk-background-center-center"><img src="../../public/img/parque_das_palmeiras.png " alt="Logo do parque"></div>
+      <div class="uk-grid uk-background-secondary">
+         <div class="uk-width-4-5"><a class="uk-text-secondary uk-text-center" href="#">Home</a></div>
+         <div class="uk-width-4-5"><a class="uk-text-secondary uk-text-center" href="#">Institucional</a></div>
+         <div class="uk-width-4-5"><a class="uk-text-secondary uk-text-center" href="#">Políticas de cookies</a></div>
+         <div class="uk-width-4-5"><a class="uk-text-secondary uk-text-center" href="#">Políticas de privacidade</a></div>
+         <div class="uk-width-1-5 un-position-top uk-background-center-center"><img src="/img/parque_das_palmeiras.png " alt="Logo do parque"></div>
       </div>
    </div>
 </template>
