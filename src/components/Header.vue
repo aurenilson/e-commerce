a982f09167bc4ea685f16718059d672cb526fa02
Eduardo Mauricio Sehn <eduardosehn0@gmail.com>
2022-11-01 17:56:56 -0300

    atualização do header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template uk-container>
     <div>
         <div class= "uk-grid">
-            <div class="uk-width-1-2"><a href=""></a></div>
+            <div class=".uk-width-1-6,.uk-width-5-6" uk-height-viewport="offset-top: true"><img src="/img/parque_das_palmeiras.png" alt="Logo do site"></div>
             <div class="uk-width-1-2"><a href="#">Home</a></div>
             <div class="uk-width-1-2"> <a href="#">Cadastro</a></div>
         </div>
