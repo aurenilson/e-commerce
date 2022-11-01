8015d9718dd3815e4e571cae2a0050e1afd2ffc1
Helena Della Giustina <helenadgiustina@gmail.com>
2022-11-01 16:06:18 -0300

    ajuste

--- src/main.js
+++ src/main.js
@@ -6,8 +6,6 @@ import "./libs/uikit/css/uikit.css";
 import "./libs/uikit/js/uikit.js";
 import "./libs/uikit/js/uikit-icons.js";
 
-import "./assets/main.css";
-
 const app = createApp(App);
 
 app.use(router);
