c52e068949aa9b8c7e5fc912637b7d9c0d14ea24
johanes-rusch <johanesrusch@gmail.com>
2022-11-02 20:15:47 -0300

    Continuação do footer (quase pronto)

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,16 +1,29 @@
 <template>
-   <div class="uk-container">
-      <div class="uk-grid uk-background-secondary">
-         <div class="uk-width-4-5"><a class="uk-text-secondary uk-text-center" href="#">Home</a></div>
-         <div class="uk-width-4-5"><a class="uk-text-secondary uk-text-center" href="#">Institucional</a></div>
-         <div class="uk-width-4-5"><a class="uk-text-secondary uk-text-center" href="#">Políticas de cookies</a></div>
-         <div class="uk-width-4-5"><a class="uk-text-secondary uk-text-center" href="#">Políticas de privacidade</a></div>
-         <div class="uk-width-1-5 un-position-top uk-background-center-center"><img src="/img/parque_das_palmeiras.png " alt="Logo do parque"></div>
+      <div>
+         <div class="uk-container uk-background-primary">
+               <img class=" uk-align-right" src="/img/parque_das_palmeiras.png " alt="Logo do parque">
+            <ul class="uk-list uk-link-text uk-width-3-4">
+               <li><a class="uk-text-secondary" href="#">Home</a></li>
+               <li><a class="uk-text-secondary" href="#">Institucional</a></li>
+               <li><a class="uk-text-secondary" href="#">Políticas de cookies</a></li>
+               <li><a class="uk-text-secondary" href="#">Políticas de privacidade</a></li>
+            </ul>
+         </div>
+         <div class="uk-container uk-background-secondary">
+            <div class="uk-margin-top">
+               <div class="uk-align-center">Copyright &copy;2022, TODOS OS DIREITOS RESERVADOS. Todo o conteúdo do site, todas as fotos, imagens, conjunto imagem, layout, trade dress, aqui veiculados são meramente ilustrativas e de propriedade exclusiva do Grupo da tarde e de seus integrantes. É vedada qualquer reprodução, total ou parcial, de qualquer elemento de identidade, sem expressa autorização. A violação de qualquer direito mencionado implicará na responsabilização cível e criminal nos termos da Lei.</div>
+            </div>
+         </div>
       </div>
-   </div>
 </template>
 <script>
    export default {
       name: 'Footer'
    }
 </script>
+<style scoped>
+/* *{
+   border: 1px solid red;
+   padding: 1px;
+} */
+</style>
