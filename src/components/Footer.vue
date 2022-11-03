3861a8776a690f0e94f6c8a897d79b24406081b1
johanes-rusch <johanesrusch@gmail.com>
2022-11-02 21:08:15 -0300

    Commit de segurança

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
       <div>
          <div class="uk-container uk-background-primary">
-               <img class=" uk-align-right" src="/img/parque_das_palmeiras.png " alt="Logo do parque">
+               <img class=" uk-align-right" src="/img/parque_das_palmeiras.png " alt="Logo do Parque">
             <ul class="uk-list uk-link-text uk-width-3-4">
                <li><a class="uk-text-secondary" href="#">Home</a></li>
                <li><a class="uk-text-secondary" href="#">Institucional</a></li>
@@ -11,7 +11,13 @@
          </div>
          <div class="uk-container uk-background-secondary">
             <div class="uk-margin-top">
-               <div class="uk-align-center">Copyright &copy;2022, TODOS OS DIREITOS RESERVADOS. Todo o conteúdo do site, todas as fotos, imagens, conjunto imagem, layout, trade dress, aqui veiculados são meramente ilustrativas e de propriedade exclusiva do Grupo da tarde e de seus integrantes. É vedada qualquer reprodução, total ou parcial, de qualquer elemento de identidade, sem expressa autorização. A violação de qualquer direito mencionado implicará na responsabilização cível e criminal nos termos da Lei.</div>
+               <div class="uk-text-center">Copyright &copy;2022, TODOS OS DIREITOS RESERVADOS. Todo o conteúdo do site, todas as fotos, imagens, conjunto imagem, layout, trade dress, aqui veiculados são meramente ilustrativas e de propriedade exclusiva do Grupo da tarde e de seus integrantes. É vedada qualquer reprodução, total ou parcial, de qualquer elemento de identidade, sem expressa autorização. A violação de qualquer direito mencionado implicará na responsabilização cível e criminal nos termos da Lei.</div>
+            </div>
+               <div class="uk-margin-top">
+               <div class="uk-text-center">
+                  <p>Powered By</p>
+                  <img src="/img/imply_logo.png" width="180px" height="150px" alt="Logo da Imply">
+               </div>
             </div>
          </div>
       </div>
