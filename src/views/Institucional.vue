6d3bb959008d66e8b5880de7708b8e964b0f21b1
Eduardo Mauricio Sehn <eduardosehn0@gmail.com>
2022-11-03 17:45:24 -0300

    pagina "institucional"

--- src/views/Institucional.vue
+++ src/views/Institucional.vue
@@ -1,7 +1,19 @@
 <template>
     <main>
-        <h1>Está a página de informações institucionais!</h1>
+        <h1>Sobre nós</h1>
     </main>
+    <div class="uk-text-justify uk-text-break">
+        <p>
+                Lorem ipsum dolor sit amet. Est rerum necessitatibus qui galisum quasi et corrupti beatae non deserunt 
+            quibusdam. Ut molestiae adipisci cum delectus modi eos nihil dolores est nostrum voluptate est aliquam 
+            galisum et ratione delectus non consequatur modi.Qui corrupti molestiae et quam voluptatibus est eveni
+            et dolor ut temporibus error id quod aspernatur et libero iusto. Ut amet architecto qui nihil voluptates 
+            est iste consequatur id vero autem ad voluptas internos qui numquam rerum ex earum similique. Sed modi 
+            nihil cum consectetur officia ut nostrum voluptas ea harum ipsum qui earum ducimus.Et eligendi iusto qui
+            iusto laboriosam ut velit asperiores ad reiciendis illum qui exercitationem voluptas sit quibusdam odio.
+            Eos culpa labore id magni ratione aut sint consectetur ex laborum reprehenderit!
+        </p>
+    </div>
 </template>
 
 <script>
