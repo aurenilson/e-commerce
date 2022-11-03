6129b2842b7cc74da508ff3f1d0d91ea346bdfad
Eduardo Mauricio Sehn <eduardosehn0@gmail.com>
2022-11-03 16:35:54 -0300

    criação da pagina "perfil"

--- src/views/Perfil.vue
+++ src/views/Perfil.vue
@@ -1,9 +1,25 @@
 <template>
     <main>
-        <h1>Está a página para visualizar e editar as informações do perfil do usuário!</h1>
+        <h4>Está a página para visualizar e editar as informações do perfil do usuário!</h4>
     </main>
+    <div class="uk-list">
+        <h1>Perfil</h1>
+        <li><label class="uk-text-secondary uk-form-label" type="text">Nome: </label></li>
+        <input type="text" class="uk-input uk-width-1-3" placeholder="Nome">
+        <li><label class="uk-text-secondary uk-form-label" type="text">CPF: </label></li>
+        <input type="text" class="uk-input uk-width-1-3" placeholder="CPF">
+        <li><label class="uk-text-secondary uk-form-label" type="text">Sexo: </label></li>
+        <input type="text" class="uk-input uk-width-1-3" placeholder="Sexo">
+        <li><label class="uk-text-secondary uk-form-label" type="text">Celular: </label></li>
+        <input type="text" class="uk-input uk-width-1-3" placeholder="Celular">
+        <li><label class="uk-text-secondary uk-form-label" type="text">Email: </label></li>
+        <input type="text" class="uk-input uk-width-1-3" placeholder="Email">
+
+        
+        
+    </div>
 </template>
 
 <script>
-
+   
 </script>
