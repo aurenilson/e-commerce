ebd8aeda58e3c4b053f30974a61576f11e7dc549
Eduardo Mauricio Sehn <eduardosehn0@gmail.com>
2022-11-03 17:30:53 -0300

    centralização do texto

--- src/views/Perfil.vue
+++ src/views/Perfil.vue
@@ -1,8 +1,5 @@
 <template>
-    <main>
-        <h4>Está a página para visualizar e editar as informações do perfil do usuário!</h4>
-    </main>
-    <div class="uk-list">
+    <div class="uk-list uk-text-center">
         <h1>Perfil</h1>
         <li><label class="uk-text-secondary uk-form-label" type="text">Nome: </label></li>
         <input type="text" class="uk-input uk-width-1-3" placeholder="Nome">
